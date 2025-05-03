bdfe0edc7a66c24e971f6a81ca3e7166dae601b7
qulanoraz <programmingmimo@gmail.com>
2025-05-03 20:52:17 +0500

    MyHashTable, HashNode classes implemented

---
+++ src/MyHashTable.java
@@ -0,0 +1,22 @@
+public class MyHashTable<K, V> {
+
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+    private HashNode<K, V>[] chainArray; // or Object[]
+    private int M = 11; // default number of chains
+    private int size;
+}
